baa86b24198542ed8bc4f3b310037a8739d2d292
Alex <97796170+pvk04@users.noreply.github.com>
2022-10-18 14:58:14 +0300

    Add files via upload

--- project/interface/js/network.js
+++ project/interface/js/network.js
@@ -1,7 +1,8 @@
 import abi from "./abi.js";
 
 export let web3, contractInstanse;
-const contractAddress = "0x43f05e973694a740f78B5690eF5D641fD411CdFd";
+// const contractAddress = "0x43f05e973694a740f78B5690eF5D641fD411CdFd";
+const contractAddress = "0xA17aDDD1CF20C497ce75Ddc76B6C9c6c7383Ec06";
 
 export function network() {
 	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
--- project/my-network.bat
+++ project/my-network.bat
@@ -1,4 +1,4 @@
 @echo off
-start geth --datadir ./geth --networkid 15 --http --http.corsdomain "*" --allow-insecure-unlock
+start geth --datadir ./geth --networkid 15 --http --http.api "personal,web3,net,eth" --http.corsdomain "*" --allow-insecure-unlock
 start geth attach \\.\pipe\geth.ipc --exec miner.start()
 start live-server ./interface
